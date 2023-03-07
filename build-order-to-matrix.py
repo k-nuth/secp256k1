0e585e1273d63c2a6332b6832765f646ae3300f5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-03-07 17:04:28 +0100

    improve build deps

--- build-order-to-matrix.py
+++ build-order-to-matrix.py
@@ -2,17 +2,40 @@ import json
 
 def main():
     matrix = {"include": []}
-    with open("build_order.json", "r") as read_file:
-        data = json.load(read_file)
-        for level in data:
-            for reference in level:
-                matrix["include"].append({"reference": reference[0]})
 
-        if len(matrix["include"]) == 0:
-            matrix["include"].append({"reference": "null"})
+#     "config":
+#         [
+#             {"name": "Linux GCC 11",
+#              "compiler": "GCC",
+#              "version": "11",
+#              "os": "ubuntu-20.04",
+#              "docker_suffix": "-ubuntu16.04"
+#             },
 
-        with open("matrix.json", "w") as write_file:
-            json.dump(matrix, write_file)
+#             {"name": "macOS apple-clang 13","compiler": "apple-clang","version": "13","os": "macos-12"}]}
+
+
+
+    with open("./ci_utils/.github/matrix.json", "r") as platform_file:
+        platform_data = json.load(platform_file)
+
+        with open("build_order.json", "r") as read_file:
+            data = json.load(read_file)
+
+            for level in data:
+                for reference in level:
+                    for platform in platform_data['config']:
+                        platform["name"] = f'{platform["name"]} - {reference[0]}'
+                        platform["reference"] = reference[0]
+                        matrix["include"].append(platform)
+
+
+            if len(matrix["include"]) == 0:
+                matrix["include"].append({"reference": "null"})
+
+    # print(matrix)
+    with open("matrix.json", "w") as write_file:
+        json.dump(matrix, write_file)
 
 if __name__ == "__main__":
     main()
